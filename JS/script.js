e48b188ab5abd6705a15623d5f9401e655030e42
Pablo Duarte <pablo@Air-de-Pablo.fibertel.com.ar>
2022-03-24 21:32:47 -0300

    jaf

--- JS/script.js
+++ JS/script.js
@@ -159,3 +159,12 @@ subs.addEventListener("keyup", function(event) {
     event.key === "Enter" && alert("Se ha suscrito a nuestro Newsletter con éxito ")
 });
 
+// Librerias
+
+Swal.fire({
+    title: 'Ingresó correctamente a ferretería PALO, bienvenido!',
+    text: 'Recuerde que puede pagar con tarjetas de crédito o MercadoPago',
+    icon: 'success',
+    confirmButtonText: 'A comprar herramientas!',
+    confirmButtonColor: '#11120f',
+})
