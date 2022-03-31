4812ea28fea0464ee7e1d74e94bfdd1b83f56dc9
Pablo Duarte <pablo@Air-de-Pablo.fibertel.com.ar>
2022-03-31 18:57:50 -0300

    kals

--- JS/script.js
+++ JS/script.js
@@ -168,3 +168,25 @@ Swal.fire({
     confirmButtonText: 'A comprar herramientas!',
     confirmButtonColor: '#11120f',
 })
+
+
+//Fetch
+
+const lista = document.querySelector('#listado')
+
+fetch('/data.json')
+    .then( (res) => res.json())
+    .then( (data) => {
+
+        data.forEach((producto) => {
+            const li = document.createElement('li')
+            li.innerHTML = `
+                <h4>${producto.nombre}</h4>
+                <p>Precio: $ ${producto.precio}</p>
+                <p>Código: ${producto.id}</p>
+                <hr/>
+            `
+   
+            lista.append(li)
+        })
+    })
